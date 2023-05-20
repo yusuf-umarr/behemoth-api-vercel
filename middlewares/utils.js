851bcc7fc7fa5b2e81e6d5ac4c6eeb312c41f328
mac <mac@macs-MacBook-Pro.local>
2023-05-20 10:24:13 +0100

    fix mail-jet

--- middlewares/utils.js
+++ middlewares/utils.js
@@ -1,19 +1,12 @@
 require("dotenv").config();
 const Mailjet = require('node-mailjet');
 
-// const mailjet = Mailjet.apiConnect(
-//   process.env.MAIL_JET_API_KEY,
-//   process.env.MAIL_JET_SECRET_KEY,
-//   {
-//     config: {},
-//     options: {}
-//   } 
-// );
 
 const mailjet = new Mailjet({
   apiKey: process.env.MAIL_JET_API_KEY ,
   apiSecret: process.env.MAIL_JET_SECRET_KEY 
 });
+
 //generating otp
 exports.generateOTP = () => {
   let otp = "";
@@ -24,9 +17,9 @@ exports.generateOTP = () => {
   return otp;
 };
 
-//mail sender aoi
-exports.mailSender = (to, subject, text, html) => {
-  const request = mailjet.post("send", { version: "v3.1" }).request({
+//mail sender 
+exports.mailSender = async(to, subject, text, html) => {
+  const request =await  mailjet.post("send", { version: "v3.1" }).request({
     Messages: [
       {
         From: {
