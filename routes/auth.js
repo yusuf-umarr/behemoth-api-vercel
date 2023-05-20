0b272087a2789eda88b4b5972e63086acd923b04
mac <mac@macs-MacBook-Pro.local>
2023-05-20 11:38:26 +0100

    implemented: get-user route

--- routes/auth.js
+++ routes/auth.js
@@ -210,6 +210,27 @@ authRouter.post("/signin", async (req, res) => {
   }
 });
 
+//get user
+authRouter.get("/get-user", async (req, res) => {
+  try {
+    const token = req.header("x-auth-token");
+    if (!token) return res.json(false);
+ 
+    const verified = jwt.verify(token, "passwordKey");
+    if (!verified) return res.json(false);
+
+    const user = await User.findById(verified.id);
+ 
+    res.json({ ...user._doc, token });
+
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+
+
+
 
 
 // const url = 'https://calling.api.sinch.com/calling/v1/callouts';
